ca44a709fad0cff792f1d063c53d5d9bad5b5425
Egor <112934317+Forehm@users.noreply.github.com>
2023-06-07 22:08:32 +0300

    Update IsStringNumeric.h

--- IsStringNumeric.h
+++ IsStringNumeric.h
@@ -1,3 +1,8 @@
+#pragma once
+#include <set>
+#include <string>
+
+
 bool IsStringNumeric(const std::string& str) 
 {
 	std::set<char> appropriate_symbols = { '0','1','2','3','4','5','6','7','8','9' };
