320615a228215bd758960188f05ab5f275d1fe24
Egor <112934317+Forehm@users.noreply.github.com>
2023-06-23 19:25:57 +0300

    Update IsStringNumeric.h

--- IsStringNumeric.h
+++ IsStringNumeric.h
@@ -5,8 +5,8 @@
 
 bool IsStringNumeric(const std::string& str) 
 {
-size_t counter_of_dots = 0;
-	std::set<char> appropriate_symbols = { '0','1','2','3','4','5','6','7','8','9' };
+ size_t counter_of_dots = 0;
+	std::set<char> appropriate_symbols = { '0','1','2','3','4','5','6','7','8','9','.' };
 
 	for (const char& symbol : str)
 	{
